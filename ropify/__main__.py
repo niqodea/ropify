313327ca88db00ee01fe02d5b87d2c7b46374f3f
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2023-10-27 15:14:20 +0200

    feat: add show-autoimports command

--- ropify/__main__.py
+++ ropify/__main__.py
@@ -1,14 +1,36 @@
+import sys
+from contextlib import redirect_stdout
 from pathlib import Path
+from typing import Callable
 
 import click
+from click.decorators import FC
 from rope.base.libutils import is_python_file, path_to_resource
 from rope.base.project import Project
+from rope.contrib.autoimport.sqlite import AutoImport
 from rope.refactor.move import create_move
 
 
 @click.group()
 def cli():
     pass
+
+
+def project_option() -> Callable[[FC], FC]:
+    return click.option(
+        "--project",
+        type=click.Path(exists=True, file_okay=False, path_type=Path),
+        default=Path.cwd(),
+        help="The project to work on.",
+    )
+
+
+def ropefolder_option() -> Callable[[FC], FC]:
+    return click.option(
+        "--ropefolder",
+        type=click.STRING,
+        help="The location of the rope folder relative to the project root.",
+    )
 
 
 @cli.command()
@@ -25,17 +47,8 @@ def cli():
     type=click.Path(exists=True, file_okay=False, path_type=Path),
     help="The path where to move the definition to.",
 )
-@click.option(
-    "--project",
-    type=click.Path(exists=True, file_okay=False, path_type=Path),
-    default=Path.cwd(),
-    help="The project to work on.",
-)
-@click.option(
-    "--ropefolder",
-    type=click.STRING,
-    help="The location of the rope folder relative to the project root.",
-)
+@project_option()
+@ropefolder_option()
 def move(
     resource: Path,
     offset: int,
@@ -52,6 +65,7 @@ def move(
     :param project: The project to work on.
     :param ropefolder: The location of the rope folder relative to the project root.
     """
+
     if ropefolder is not None:
         rope_project = Project(project, ropefolder=ropefolder)
     else:
@@ -78,6 +92,41 @@ def move(
     changes = move.get_changes(rope_destination)
     rope_project.do(changes)
     click.echo(f"Definition of `{object_name}` moved to: {rope_destination.path}")
+
+
+@cli.command()
+@click.argument("name", type=click.STRING)
+@project_option()
+@ropefolder_option()
+def show_autoimports(name: str, project: Path, ropefolder: str | None) -> None:
+    """
+    Print the candidate autoimports for a given name to stdout.
+
+    :param name: The name to search for.
+    :param project: The project to work on.
+    :param ropefolder: The location of the rope folder relative to the project root.
+    """
+
+    if ropefolder is not None:
+        rope_project = Project(project, ropefolder=ropefolder)
+    else:
+        rope_project = Project(project)
+
+    autoimport = AutoImport(rope_project, memory=False)
+
+    with redirect_stdout(sys.stderr):
+        autoimport.generate_cache()
+        autoimport.generate_modules_cache()
+
+    raw_modules: list[str] = autoimport.get_modules(name)
+    modules = [m for m in raw_modules if not m.startswith("site-packages.")]
+
+    if len(modules) == 0:
+        click.echo(f"No modules found for {name}")
+        exit(1)
+
+    for module in modules:
+        click.echo(f"from {module} import {name}")
 
 
 if __name__ == "__main__":
